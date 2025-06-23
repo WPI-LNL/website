f6660b1882f2af11f595ffed237b78d186ae02b5
Ben Feinberg <66845228+benfeinberg@users.noreply.github.com>
2025-06-23 14:21:40 -0400

    initial base template and template rendering script

---
+++ templates/render.py
@@ -0,0 +1,46 @@
+"""
+usage: python3 templates/render.py
+
+Looks for template files in the same directory as this script
+and outputs html to the current working directory (where it was run from).
+WARNING: it will overwrite existing files in the working directory.
+"""
+
+
+import django
+from django.conf import settings
+from django.template import Template, Context
+from pathlib import Path
+
+template_dir = Path(__file__).parent
+
+settings.configure(
+    TEMPLATES = [{
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [template_dir],
+    }]
+)
+django.setup()
+
+pages = {
+    # url : template_filename
+    "/index.html" : "index.html",
+    "/about.html" : "about.html",
+    "/services/lighting.html" : "lighting.html",
+}
+
+
+for url, template_filename in pages.items():
+    template = None
+
+    try:
+        with open(template_dir / template_filename) as template_file:
+            template = Template(template_file.read())
+    except FileNotFoundError:
+        print(f'Could not find the template: {template_filename}. Skipping this page.')
+        continue
+
+    html = template.render(Context())
+    
+    with open(Path('.' + url), 'w') as dest_file:
+        dest_file.write(html)
